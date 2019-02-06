b00d365d6cbc9d9f27eb9af90e165f3adda6764f
McJty <mcjty1@gmail.com>
2019-02-06 06:09:39 +0100

    Basic mod structure

---
+++ src/main/java/mcjty/fxcontrol/FxControl.java
@@ -0,0 +1,74 @@
+package mcjty.fxcontrol;
+
+
+import mcjty.fxcontrol.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.*;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = FxControl.MODID, name = FxControl.MODNAME,
+        dependencies =
+                "after:forge@[" + FxControl.MIN_FORGE11_VER + ",)",
+        version = FxControl.VERSION,
+        acceptedMinecraftVersions = "[1.12,1.13)",
+        acceptableRemoteVersions = "*")
+public class FxControl {
+
+    public static final String MODID = "fxcontrol";
+    public static final String MODNAME = "FxControl";
+    public static final String VERSION = "3.8.3";
+    public static final String MIN_FORGE11_VER = "13.19.0.2176";
+
+    @SidedProxy(clientSide = "mcjty.fxcontrol.proxy.ClientProxy", serverSide = "mcjty.fxcontrol.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static FxControl instance;
+
+    public static Logger logger;
+
+    public static boolean lostcities = false;
+    public static boolean gamestages = false;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+        logger = event.getModLog();
+        proxy.preInit(event);
+
+        lostcities = Loader.isModLoaded("lostcities");
+        gamestages = Loader.isModLoaded("gamestages");
+
+//        if (lostcities) {
+//            LostCitySupport.register();
+//        }
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
+    }
+
+    @Mod.EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+//        event.registerServerCommand(new CmdReload());
+//        event.registerServerCommand(new CmdDebug());
+//        event.registerServerCommand(new CmdLoadSpawn());
+//        event.registerServerCommand(new CmdLoadPotentialSpawn());
+//        event.registerServerCommand(new CmdLoadSummonAid());
+//        event.registerServerCommand(new CmdLoadLoot());
+//        event.registerServerCommand(new CmdShowMobs());
+//        event.registerServerCommand(new CmdKillMobs());
+    }
+
+    @Mod.EventHandler
+    public void serverStopped(FMLServerStoppedEvent event) {
+//        StructureCache.CACHE.clean();
+    }
+}
---
+++ src/main/java/mcjty/fxcontrol/proxy/CommonProxy.java
@@ -0,0 +1,45 @@
+package mcjty.fxcontrol.proxy;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.util.concurrent.Callable;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+        //PacketHandler.registerMessages("fxcontrol");
+//        ConfigSetup.preInit(e);
+//        RulesManager.setRulePath(e.getModConfigurationDirectory());
+    }
+
+    public void init(FMLInitializationEvent e) {
+//        MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers());
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+//        ConfigSetup.postInit();
+//        RulesManager.readRules();
+    }
+
+    public World getClientWorld() {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public EntityPlayer getClientPlayer() {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public <V> ListenableFuture<V> addScheduledTaskClient(Callable<V> callableToSchedule) {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+}
