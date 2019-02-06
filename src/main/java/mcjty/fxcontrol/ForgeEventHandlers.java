cb9cc2692ee2778b470e69b29c7de98489f7d20d
McJty <mcjty1@gmail.com>
2019-02-06 18:33:18 +0100

    Fix so that the counters are correctly handled per player

--- src/main/java/mcjty/fxcontrol/ForgeEventHandlers.java
+++ src/main/java/mcjty/fxcontrol/ForgeEventHandlers.java
@@ -6,10 +6,14 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ForgeEventHandlers {
 
     public static boolean debug = false;
-    public static int tickCounter = 0;
+
+    public static Map<Integer, Integer> tickCounters = new HashMap<>();
 
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
@@ -20,7 +24,12 @@ public class ForgeEventHandlers {
             return;
         }
 
-        tickCounter++;
+        int id = event.player.getEntityId();
+        if (!tickCounters.containsKey(id)) {
+            tickCounters.put(id, 0);
+        }
+        int tickCounter = tickCounters.get(id) + 1;
+        tickCounters.put(id, tickCounter);
         int i = 0;
         for (EffectRule rule : RulesManager.effectRules) {
             if (tickCounter % rule.getTimeout() == 0 && rule.match(event)) {
