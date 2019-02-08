6cdafefcdc9d70aa43d387b8837309875664ce28
McJty <mcjty1@gmail.com>
2019-02-08 12:27:46 +0100

    Refactoring to generalize the action system a bit more

--- src/main/java/mcjty/fxcontrol/rules/EffectRule.java
+++ src/main/java/mcjty/fxcontrol/rules/EffectRule.java
@@ -15,12 +15,7 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.Level;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 import java.util.function.Consumer;
 
 import static mcjty.fxcontrol.rules.support.RuleKeys.*;
@@ -136,101 +131,6 @@ public class EffectRule extends RuleBase<RuleBase.EventGetter> {
     public int getTimeout() {
         return timeout;
     }
-
-    @Override
-    protected void addActions(AttributeMap map) {
-        super.addActions(map);
-
-        if (map.has(ACTION_FIRE)) {
-            addFireAction(map);
-        }
-        if (map.has(ACTION_CLEAR)) {
-            addClearAction(map);
-        }
-        if (map.has(ACTION_DAMAGE)) {
-            addDamageAction(map);
-        }
-    }
-
-    private static Map<String, DamageSource> damageMap = null;
-    private static void addSource(DamageSource source) {
-        damageMap.put(source.getDamageType(), source);
-    }
-
-    private void createDamageMap() {
-        if (damageMap == null) {
-            damageMap = new HashMap<>();
-            addSource(DamageSource.IN_FIRE);
-            addSource(DamageSource.LIGHTNING_BOLT);
-            addSource(DamageSource.ON_FIRE);
-            addSource(DamageSource.LAVA);
-            addSource(DamageSource.HOT_FLOOR);
-            addSource(DamageSource.IN_WALL);
-            addSource(DamageSource.CRAMMING);
-            addSource(DamageSource.DROWN);
-            addSource(DamageSource.STARVE);
-            addSource(DamageSource.CACTUS);
-            addSource(DamageSource.FALL);
-            addSource(DamageSource.FLY_INTO_WALL);
-            addSource(DamageSource.OUT_OF_WORLD);
-            addSource(DamageSource.GENERIC);
-            addSource(DamageSource.MAGIC);
-            addSource(DamageSource.WITHER);
-            addSource(DamageSource.ANVIL);
-            addSource(DamageSource.FALLING_BLOCK);
-            addSource(DamageSource.DRAGON_BREATH);
-            addSource(DamageSource.FIREWORKS);
-        }
-    }
-
-    private void addDamageAction(AttributeMap map) {
-        String damage = map.get(ACTION_DAMAGE);
-        createDamageMap();
-        String[] split = StringUtils.split(damage, "=");
-        DamageSource source = damageMap.get(split[0]);
-        if (source == null) {
-            FxControl.logger.log(Level.ERROR, "Can't find damage source '" + split[0] + "'!");
-            return;
-        }
-        float amount = 1.0f;
-        if (split.length > 1) {
-            amount = Float.parseFloat(split[1]);
-        }
-
-        float finalAmount = amount;
-        actions.add(event -> {
-            EntityLivingBase living = event.getEntityLiving();
-            if (living != null) {
-                living.attackEntityFrom(source, finalAmount);
-            }
-        });
-    }
-
-
-    private void addClearAction(AttributeMap map) {
-        Boolean clear = map.get(ACTION_CLEAR);
-        if (clear) {
-            actions.add(event -> {
-                EntityLivingBase living = event.getEntityLiving();
-                if (living != null) {
-                    living.clearActivePotions();
-                }
-            });
-        }
-    }
-
-    private void addFireAction(AttributeMap map) {
-        Integer fireAction = map.get(ACTION_FIRE);
-        actions.add(event -> {
-            EntityLivingBase living = event.getEntityLiving();
-            if (living != null) {
-                living.attackEntityFrom(DamageSource.ON_FIRE, 0.1f);
-                living.setFire(fireAction);
-            }
-        });
-    }
-
-    private static Random rnd = new Random();
 
     public boolean match(TickEvent.PlayerTickEvent event) {
         return ruleEvaluator.match(event, EVENT_QUERY);
--- src/main/java/mcjty/fxcontrol/rules/support/RuleKeys.java
+++ src/main/java/mcjty/fxcontrol/rules/support/RuleKeys.java
@@ -1,13 +1,6 @@
 package mcjty.fxcontrol.rules.support;
 
 import mcjty.tools.rules.CommonRuleKeys;
-import mcjty.tools.typed.Key;
-import mcjty.tools.typed.Type;
 
 public interface RuleKeys extends CommonRuleKeys {
-
-    // Outputs
-    Key<Integer> ACTION_FIRE = Key.create(Type.INTEGER, "fire");
-    Key<Boolean> ACTION_CLEAR = Key.create(Type.BOOLEAN, "clear");
-    Key<String> ACTION_DAMAGE = Key.create(Type.STRING, "damage");
 }
