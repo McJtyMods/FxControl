b25cd5d3d5517a21b8267da24dec6bc095084c28
McJty <mcjty1@gmail.com>
2019-02-11 05:30:29 +0100

    Removed 'blockup' command and instead add a more general 'blockoffset' command that works together with the 'block' command

--- src/main/java/mcjty/fxcontrol/rules/EffectRule.java
+++ src/main/java/mcjty/fxcontrol/rules/EffectRule.java
@@ -105,7 +105,7 @@ public class EffectRule extends RuleBase<RuleBase.EventGetter> {
                 .attribute(Attribute.createMulti(CHARM))
 
                 .attribute(Attribute.createMulti(BLOCK))
-                .attribute(Attribute.createMulti(BLOCKUP))
+                .attribute(Attribute.create(BLOCKOFFSET))
                 .attribute(Attribute.createMulti(HELMET))
                 .attribute(Attribute.createMulti(CHESTPLATE))
                 .attribute(Attribute.createMulti(LEGGINGS))
--- src/main/java/mcjty/fxcontrol/rules/RightClickRule.java
+++ src/main/java/mcjty/fxcontrol/rules/RightClickRule.java
@@ -106,7 +106,7 @@ public class RightClickRule extends RuleBase<RuleBase.EventGetter> {
                 .attribute(Attribute.createMulti(CHARM))
 
                 .attribute(Attribute.createMulti(BLOCK))
-                .attribute(Attribute.createMulti(BLOCKUP))
+                .attribute(Attribute.create(BLOCKOFFSET))
                 .attribute(Attribute.createMulti(HELMET))
                 .attribute(Attribute.createMulti(CHESTPLATE))
                 .attribute(Attribute.createMulti(LEGGINGS))
