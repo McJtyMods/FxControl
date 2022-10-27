26f4e4c0d186c3db9cff5325a95968c28fee9ef2
McJty <mcjty1@gmail.com>
2022-10-27 14:23:35 +0200

    Implemented gamestage support

--- src/main/java/mcjty/fxcontrol/compat/GameStageSupport.java
+++ src/main/java/mcjty/fxcontrol/compat/GameStageSupport.java
@@ -5,29 +5,25 @@ import net.minecraft.world.entity.player.Player;
 public class GameStageSupport {
 
     public static boolean hasGameStage(Player player, String stage) {
-        // @todo 1.18
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            return stageData.hasStage(stage);
-//        } else {
-//            return false;
-//        }
-        return false;
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            return stageData.hasStage(stage);
+        } else {
+            return false;
+        }
     }
 
     public static void addGameStage(Player player, String stage) {
-        // @todo 1.18
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            stageData.addStage(stage);
-//        }
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            stageData.addStage(stage);
+        }
     }
 
     public static void removeGameStage(Player player, String stage) {
-        // @todo 1.18
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            stageData.addStage(stage);
-//        }
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            stageData.addStage(stage);
+        }
     }
 }
